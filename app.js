1e777d4c1c2cd7c508fff4341e126f67efc1d9b4
Thibault Jan Beyer <thibault.beyer@gmail.com>
2023-06-21 11:00:24 +0200

    up time

--- app.js
+++ app.js
@@ -290,7 +290,7 @@ async function writeUserMessage(channel, thread, member, answers) {
   console.info("The current time on this server is:", new Date());
 
   // Schedule a function to run at 7 AM every working day
-  schedule.scheduleJob("00 09 * * 1-5", function () {
+  schedule.scheduleJob("02 09 * * 1-5", function () {
     startStandup({ channel: process.env.CHANNEL_ID });
   });
 
