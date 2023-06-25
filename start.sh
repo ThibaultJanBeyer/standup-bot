890c7256d0b792c3a6b76da232ab550c7e4c277e
Thibault Jan Beyer <thibault.beyer@gmail.com>
2023-06-25 23:30:34 +0200

    remove unused images

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ HOST="standupbotcom"
 IMG_NAME="ssb"
 IMG_VER=`cat ${CONFIG} | grep '"version"' | awk 'BEGIN{FS="\""} {print $4}'`
 
-docker rmi $(docker images -aq)
 docker pull ${HOST}/${IMG_NAME}:${IMG_VER}
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
+# docker rmi $(docker images -aq)
 docker run -d -p 3000:3000 --restart unless-stopped --name ssb ${HOST}/${IMG_NAME}:${IMG_VER}
-docker rmi $(docker images -aq)
+docker image prune
