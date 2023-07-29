051d1c667900f425c136c6de987c91a6c8b56a52
Thibault Jan Beyer <thibault.beyer@gmail.com>
2023-07-29 17:03:12 +0100

    fix cronpicker

--- apps/web/src/app/(pages)/standups/StandupsFormFields.tsx
+++ apps/web/src/app/(pages)/standups/StandupsFormFields.tsx
@@ -3,13 +3,19 @@
 import React, { useEffect, useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as Form from "@radix-ui/react-form";
-import { SubmitHandler, useFieldArray, useForm } from "react-hook-form";
-import { Cron } from "react-js-cron";
+import {
+  FormProvider,
+  SubmitHandler,
+  useFieldArray,
+  useForm,
+} from "react-hook-form";
 import * as zod from "zod";
 
 import { Button } from "@ssb/ui/button";
 import { DeleteIcon } from "@ssb/ui/icons";
 import { Input } from "@ssb/ui/input";
+
+import { CronPicker } from "@/components/CronPicker";
 
 const schema = {
   name: zod.string().nonempty({ message: "Name is required" }),
@@ -67,8 +73,6 @@ type Props = {
 export default ({ onSubmit, data, children }: Props) => {
   const [channels, setChannels] = useState<Data[]>([]);
   const [users, setUsers] = useState<Data[]>([]);
-  const [scheduleCronValue, setScheduleCronValue] = useState("0 5 * * 1-5");
-  const [summaryCronValue, setSummaryCronValue] = useState("0 9 * * 1-5");
   const form = useForm({
     resolver: zodResolver(zod.object(schema).strict()),
     mode: "onChange",
@@ -120,194 +124,174 @@ export default ({ onSubmit, data, children }: Props) => {
 
   const errors = form.formState.errors;
 
+  console.log("change", form.getValues());
+
   return (
-    <Form.Root onSubmit={form.handleSubmit(onSubmit)}>
-      <Form.Field name="name" className="mb-10">
-        <Form.Label className="mb-2 block font-bold">
-          Name
-          {Boolean(errors.name?.message) && (
-            <Form.Message className="font-normal text-red-600">
-              {` (${errors.name?.message})`}
-            </Form.Message>
-          )}
-        </Form.Label>
-        <Form.Control asChild>
-          <Input {...form.register("name")} />
-        </Form.Control>
-      </Form.Field>
-      <Form.Field name="scheduleCron" className="mb-10">
-        <Form.Label className="mb-2 block font-bold">
-          Select time to send-out questionnaire (in UTC):
-          {Boolean(errors.scheduleCron?.message) && (
-            <Form.Message className="font-normal text-red-600">
-              {` (${errors.scheduleCron?.message})`}
-            </Form.Message>
-          )}
-        </Form.Label>
-        {Boolean(errors.scheduleCron?.message) && (
-          <Form.Message className="text-red-600">
-            {`(${errors.scheduleCron?.message})`}
-          </Form.Message>
-        )}
-        <Form.Control asChild>
-          <>
-            <Cron
-              clockFormat={"24-hour-clock"}
-              defaultPeriod="week"
-              leadingZero={true}
-              className={"cron-picker"}
-              value={scheduleCronValue}
-              clearButton={false}
-              setValue={setScheduleCronValue}
-            />
-            <Input
-              {...form.register("scheduleCron")}
-              value={scheduleCronValue}
-              // style={{ visibility: "hidden", height: 0 }}
-              // aria-hidden="true"
-            />
-          </>
-        </Form.Control>
-      </Form.Field>
-      <Form.Field name="summaryCron" className="mb-10">
-        <Form.Label className="mb-2 block font-bold">
-          Select time to send-out summary (in UTC):
-          {Boolean(errors.summaryCron?.message) && (
-            <Form.Message className="font-normal text-red-600">
-              {` (${errors.summaryCron?.message})`}
-            </Form.Message>
-          )}
-        </Form.Label>
-        <Form.Control asChild>
-          <>
-            <Cron
-              clockFormat={"24-hour-clock"}
-              defaultPeriod="week"
-              leadingZero={true}
-              className={"cron-picker"}
-              value={summaryCronValue}
-              clearButton={false}
-              setValue={setSummaryCronValue}
-            />
-            <Input
-              {...form.register("summaryCron")}
-              // value={summaryCronValue}
-              // style={{ visibility: "hidden", height: 0 }}
-              // aria-hidden="true"
-            />
-          </>
-        </Form.Control>
-      </Form.Field>
-      <Form.Field name="questions" className="mb-10">
-        <Form.Label className="mb-2 block font-bold">
-          Questions the BOT is asking:
-          {Boolean(errors.questions?.message) && (
-            <Form.Message className="font-normal text-red-600">
-              {` (${errors.questions?.message})`}
-            </Form.Message>
-          )}
-        </Form.Label>
-        {questionFields.map((field, index) => (
-          <React.Fragment key={field.id}>
-            {Boolean((errors as any).questions?.[index]?.value?.message) && (
-              <Form.Message className="text-red-600">
-                {(errors as any).questions?.[index]?.value?.message}
+    <FormProvider {...form}>
+      <Form.Root onSubmit={form.handleSubmit(onSubmit)}>
+        <Form.Field name="name" className="mb-10">
+          <Form.Label className="mb-2 block font-bold">
+            Name
+            {Boolean(errors.name?.message) && (
+              <Form.Message className="font-normal text-red-600">
+                {` (${errors.name?.message})`}
               </Form.Message>
             )}
-            <div
-              key={field.id}
-              className={`${
-                index === questionFields.length - 1 ? "" : "mb-2"
-              } grid grid-cols-[1fr_auto] gap-2`}
-            >
-              <Form.Control asChild>
-                <Input
-                  key={field.id} // important to include key with field's id
-                  {...form.register(`questions.${index}.value`)}
-                />
-              </Form.Control>
-              <Button
-                type="button"
-                variant="ghost"
-                onClick={() => remove(index)}
-              >
-                <DeleteIcon>Delete</DeleteIcon>
-              </Button>
-            </div>
-          </React.Fragment>
-        ))}
-        <Button
-          type="button"
-          variant="outline"
-          size="sm"
-          className="mt-2"
-          onClick={() => append({ value: "" })}
-        >
-          Add Question
-        </Button>
-      </Form.Field>
-      <Form.Field name="channelId" className="mb-10">
-        <Form.Label className="mb-2 block font-bold">
-          Channel (where the summary gets posted)
-          {Boolean(errors.channelId?.message) && (
-            <Form.Message className="font-normal text-red-600">
-              {` (${errors.channelId?.message})`}
-            </Form.Message>
-          )}
-        </Form.Label>
-        <Form.Control asChild>
-          {channels.length ? (
-            <select
-              {...form.register("channelId")}
-              className="bg-default block w-full p-3"
-              style={{ borderBottom: "1px dotted rgba(100,100,100,1)" }}
-              defaultValue={selectedChannel}
-            >
-              <option key={""} value={""}>
-                Select a channel
-              </option>
-              {channels.map((channel) => (
-                <option key={channel.slackId} value={channel.slackId}>
-                  {channel.name}
-                </option>
-              ))}
-            </select>
-          ) : (
-            "Loading..."
-          )}
-        </Form.Control>
-      </Form.Field>
-      <Form.Field name="members" className="mb-10">
-        <Form.Label className="mb-2 block font-bold">
-          Select users to participate in this standup
-          {Boolean(errors.members?.message) && (
-            <Form.Message className="font-normal text-red-600">
-              {` (${errors.members?.message})`}
-            </Form.Message>
-          )}
-        </Form.Label>
-        {!users.length ? (
-          <div>
-            <em>Select a channel first!</em>
-          </div>
-        ) : (
+          </Form.Label>
           <Form.Control asChild>
-            <select
-              {...form.register("members")}
-              multiple
-              className="bg-default block w-full rounded-md p-3"
-              style={{ borderBottom: "1px dotted rgba(100,100,100,1)" }}
-            >
-              {users.map((user) => (
-                <option key={user.slackId} value={user.slackId}>
-                  {user.name}
-                </option>
-              ))}
-            </select>
+            <Input {...form.register("name")} />
           </Form.Control>
-        )}
-      </Form.Field>
-      {children}
-    </Form.Root>
+        </Form.Field>
+        <Form.Field name="scheduleCron" className="mb-10">
+          <Form.Label className="mb-2 block font-bold">
+            Select time to send-out questionnaire (in UTC):
+            {Boolean(errors.scheduleCron?.message) && (
+              <Form.Message className="font-normal text-red-600">
+                {` (${errors.scheduleCron?.message})`}
+              </Form.Message>
+            )}
+          </Form.Label>
+          {Boolean(errors.scheduleCron?.message) && (
+            <Form.Message className="text-red-600">
+              {`(${errors.scheduleCron?.message})`}
+            </Form.Message>
+          )}
+          <Form.Control asChild>
+            <CronPicker
+              value={form.getValues().scheduleCron}
+              onChange={(val: string) => form.setValue("scheduleCron", val)}
+              registered={form.register("scheduleCron")}
+            />
+          </Form.Control>
+        </Form.Field>
+        <Form.Field name="summaryCron" className="mb-10">
+          <Form.Label className="mb-2 block font-bold">
+            Select time to send-out summary (in UTC):
+            {Boolean(errors.summaryCron?.message) && (
+              <Form.Message className="font-normal text-red-600">
+                {` (${errors.summaryCron?.message})`}
+              </Form.Message>
+            )}
+          </Form.Label>
+          <Form.Control asChild>
+            <CronPicker
+              value={form.getValues().summaryCron}
+              onChange={(val: string) => form.setValue("summaryCron", val)}
+              registered={form.register("summaryCron")}
+            />
+          </Form.Control>
+        </Form.Field>
+        <Form.Field name="questions" className="mb-10">
+          <Form.Label className="mb-2 block font-bold">
+            Questions the BOT is asking:
+            {Boolean(errors.questions?.message) && (
+              <Form.Message className="font-normal text-red-600">
+                {` (${errors.questions?.message})`}
+              </Form.Message>
+            )}
+          </Form.Label>
+          {questionFields.map((field, index) => (
+            <React.Fragment key={field.id}>
+              {Boolean((errors as any).questions?.[index]?.value?.message) && (
+                <Form.Message className="text-red-600">
+                  {(errors as any).questions?.[index]?.value?.message}
+                </Form.Message>
+              )}
+              <div
+                key={field.id}
+                className={`${
+                  index === questionFields.length - 1 ? "" : "mb-2"
+                } grid grid-cols-[1fr_auto] gap-2`}
+              >
+                <Form.Control asChild>
+                  <Input
+                    key={field.id} // important to include key with field's id
+                    {...form.register(`questions.${index}.value`)}
+                  />
+                </Form.Control>
+                <Button
+                  type="button"
+                  variant="ghost"
+                  onClick={() => remove(index)}
+                >
+                  <DeleteIcon>Delete</DeleteIcon>
+                </Button>
+              </div>
+            </React.Fragment>
+          ))}
+          <Button
+            type="button"
+            variant="outline"
+            size="sm"
+            className="mt-2"
+            onClick={() => append({ value: "" })}
+          >
+            Add Question
+          </Button>
+        </Form.Field>
+        <Form.Field name="channelId" className="mb-10">
+          <Form.Label className="mb-2 block font-bold">
+            Channel (where the summary gets posted)
+            {Boolean(errors.channelId?.message) && (
+              <Form.Message className="font-normal text-red-600">
+                {` (${errors.channelId?.message})`}
+              </Form.Message>
+            )}
+          </Form.Label>
+          <Form.Control asChild>
+            {channels.length ? (
+              <select
+                {...form.register("channelId")}
+                className="bg-default block w-full p-3"
+                style={{ borderBottom: "1px dotted rgba(100,100,100,1)" }}
+                defaultValue={selectedChannel}
+              >
+                <option key={""} value={""}>
+                  Select a channel
+                </option>
+                {channels.map((channel) => (
+                  <option key={channel.slackId} value={channel.slackId}>
+                    {channel.name}
+                  </option>
+                ))}
+              </select>
+            ) : (
+              "Loading..."
+            )}
+          </Form.Control>
+        </Form.Field>
+        <Form.Field name="members" className="mb-10">
+          <Form.Label className="mb-2 block font-bold">
+            Select users to participate in this standup
+            {Boolean(errors.members?.message) && (
+              <Form.Message className="font-normal text-red-600">
+                {` (${errors.members?.message})`}
+              </Form.Message>
+            )}
+          </Form.Label>
+          {!users.length ? (
+            <div>
+              <em>Select a channel first!</em>
+            </div>
+          ) : (
+            <Form.Control asChild>
+              <select
+                {...form.register("members")}
+                multiple
+                className="bg-default block w-full rounded-md p-3"
+                style={{ borderBottom: "1px dotted rgba(100,100,100,1)" }}
+              >
+                {users.map((user) => (
+                  <option key={user.slackId} value={user.slackId}>
+                    {user.name}
+                  </option>
+                ))}
+              </select>
+            </Form.Control>
+          )}
+        </Form.Field>
+        {children}
+      </Form.Root>
+    </FormProvider>
   );
 };
--- apps/web/src/components/CronPicker.tsx
+++ apps/web/src/components/CronPicker.tsx
@@ -1,21 +1,42 @@
-export const CronPicker = () => {
+"use-client";
+
+import { useState } from "react";
+import { UseFormRegisterReturn, useWatch } from "react-hook-form";
+import { Cron } from "react-js-cron";
+
+import { Button } from "@ssb/ui/button";
+import { Edit3Icon } from "@ssb/ui/icons";
+import { Input } from "@ssb/ui/input";
+
+type Props = {
+  registered: UseFormRegisterReturn<string>;
+  onChange: (val: string) => void;
+};
+
+export const CronPicker = ({ registered, onChange }: Props) => {
+  const [showRaw, setShowRaw] = useState(false);
+  const value = useWatch({ name: registered.name });
   return (
-    <>
+    <div className="grid grid-cols-[1fr_auto] gap-2">
       <Cron
         clockFormat={"24-hour-clock"}
         defaultPeriod="week"
         leadingZero={true}
-        className={"cron-picker"}
-        value={scheduleCronValue}
+        className={`${showRaw ? "hidden" : ""} cron-picker`}
         clearButton={false}
-        setValue={setScheduleCronValue}
+        value={value}
+        setValue={onChange}
       />
-      <Input
-        {...form.register("scheduleCron")}
-        // value={scheduleCronValue}
-        // style={{ visibility: "hidden", height: 0 }}
-        // aria-hidden="true"
-      />
-    </>
+      <Input {...registered} className={showRaw ? "" : "hidden"} />
+      <Button
+        type="button"
+        variant="ghost"
+        onClick={() => {
+          setShowRaw(!showRaw);
+        }}
+      >
+        <Edit3Icon>Edit Raw Cron</Edit3Icon>
+      </Button>
+    </div>
   );
 };
