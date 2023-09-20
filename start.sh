f28372fd66aa3e574402ee8e467cbcf84246a6e4
Thibault Jan Beyer <thibault.beyer@gmail.com>
2023-09-20 19:43:32 +0200

    update start script

--- start.sh
+++ start.sh
@@ -5,11 +5,12 @@ CONFIG="./package.json"
 IMG_NAME="ssb"
 IMG_VER=`cat ${CONFIG} | grep '"version"' | awk 'BEGIN{FS="\""} {print $4}'`
 
-docker pull ${DOCKERHUB_USERNAME}/${IMG_NAME}:${IMG_VER}
 if [[ $(docker ps -aqf name=${IMG_NAME}) ]]; then
   docker stop ${IMG_NAME}
   docker rm ${IMG_NAME}
+  docker rmi ${IMG_NAME}
+  docker image prune -af
 fi
-# docker rmi $(docker images -aq)
+docker pull ${DOCKERHUB_USERNAME}/${IMG_NAME}:${IMG_VER}
 docker run -d -p 127.0.0.1:3000:3000 -p 127.0.0.1:3001:3001 --restart unless-stopped --name ${IMG_NAME} ${DOCKERHUB_USERNAME}/${IMG_NAME}:${IMG_VER}
 docker image prune -af
